ca3ac75278a31488ce91ad5a920b159627035148
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-18 17:42:21 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,9 +10,14 @@ Olá mundo*
 # st.line_chart(df)
 
 url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
+# Definindo cabeçalhos para a requisição
+headers = {
+    "Accept": "application/json"
+}
+
 try:
     # Requisição para obter os dados JSON
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()  # Verificar se a requisição foi bem-sucedida
 
     # Tentar decodificar o JSON
@@ -25,7 +30,7 @@ try:
     df['count'] = pd.to_numeric(df['count'], errors='coerce')
 
     # Mostrar o DataFrame
-    st.write("15.23", df)
+    st.write("## Patents Data", df)
 
     # Exibir o gráfico de linhas
     st.line_chart(df.set_index('divisao')['count'])
