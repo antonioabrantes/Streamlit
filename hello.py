70e8b3f6143489fba5ca2a3defea6fe91fa73bd5
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-18 17:43:38 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,8 +12,10 @@ Olá mundo*
 url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
 # Definindo cabeçalhos para a requisição
 headers = {
-    "Accept": "application/json"
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
+
 
 try:
     # Requisição para obter os dados JSON
