44eeae2c7875129b551f7edd033f3971251e2e0e
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-18 17:40:51 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,21 +6,35 @@ st.write("""
 # My first app
 Olá mundo* 
 """)
- 
-url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
-response = requests.get(url)
-data = response.json()
-
-df = pd.DataFrame(data['patents'])
-# Verificar e converter a coluna 'count' para inteiro
-df['count'] = pd.to_numeric(df['count'], errors='coerce')
-
-
 # df = pd.read_csv("my_data.csv")
 # st.line_chart(df)
 
-# Mostrar o DataFrame
-st.write("15.23", df)
+url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
+try:
+    # Requisição para obter os dados JSON
+    response = requests.get(url)
+    response.raise_for_status()  # Verificar se a requisição foi bem-sucedida
 
-# Exibir o gráfico de linhas
-st.line_chart(df.set_index('divisao')['count'])
+    # Tentar decodificar o JSON
+    data = response.json()
+
+    # Carregar os dados JSON em um DataFrame
+    df = pd.DataFrame(data['patents'])
+
+    # Verificar e converter a coluna 'count' para inteiro
+    df['count'] = pd.to_numeric(df['count'], errors='coerce')
+
+    # Mostrar o DataFrame
+    st.write("15.23", df)
+
+    # Exibir o gráfico de linhas
+    st.line_chart(df.set_index('divisao')['count'])
+
+except requests.exceptions.HTTPError as http_err:
+    st.error(f"HTTP error occurred: {http_err}")
+except requests.exceptions.RequestException as req_err:
+    st.error(f"Error occurred during request: {req_err}")
+except ValueError as json_err:
+    st.error(f"JSON decode error: {json_err}")
+except Exception as err:
+    st.error(f"An unexpected error occurred: {err}")
