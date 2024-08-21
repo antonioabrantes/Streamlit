84527ba369a88fedb131641bdd43c52716ea9037
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-08-21 10:37:48 -0300

    oi

--- hello.py
+++ hello.py
@@ -1,23 +1,41 @@
 import os
 from dotenv import load_dotenv
-#from langchain_openai import ChatOpenAI
-#from langchain_core.output_parsers import StrOutputParser
-#from langchain_core.prompts import ChatPromptTemplate
-#from langchain.chains import ConversationChain
-
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 import streamlit as st
 
 # Captura os parâmetros da URL
-# http://localhost:8501/?param1=valor1&param2=valor2
+# http://localhost:8501/?numero=valor1&doc=valor2
 query_params = st.experimental_get_query_params()
 
 # Acessa os valores dos parâmetros
-p1 = query_params.get('param1', [''])[0]
-p2 = query_params.get('param2', [''])[0]
+numero = query_params.get('numero', [''])[0]
+doc = query_params.get('doc', [''])[0]
 
 # Exibir os valores no Streamlit
-st.write(f"Valor de param1: {p1}")
-st.write(f"Valor de param2: {p2}")
+st.write(f"Numero: {numero}")
+st.write(f"Doc: {doc}")
+
+load_dotenv()
+open_api_key = os.getenv("OPENAI_API_KEY2")
+st.write(open_api_key)
+
+llm = ChatOpenAI(api_key=open_api_key)
+out_parser = StrOutputParser()
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Você é um assistente virtual."),
+        ("user", "{user_input}"),
+    ])
+
+chain = prompt | llm | out_parser
+
+query = "Qual a capital do Brasil?"
+#query = texto
+resposta = chain.invoke({"user_input":f"{query}"})
+st.write(resposta)
+
 
 #import streamlit as st
 #import json
