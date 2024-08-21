cb93218259d23ce7c91c6b428e874a9f326eaf39
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-08-21 09:27:10 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import ConversationChain
+#from langchain_openai import ChatOpenAI
+#from langchain_core.output_parsers import StrOutputParser
+#from langchain_core.prompts import ChatPromptTemplate
+#from langchain.chains import ConversationChain
 
 
 app = Flask(__name__)
